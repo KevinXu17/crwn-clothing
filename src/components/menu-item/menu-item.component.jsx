79691ca3e509d079cfbcaf86d7203ea2013bfda1
KevinXu17 <xuzhaozhishu@gmail.com>
2021-07-07 18:14:59 -0700

    1) styling menu-item with hover

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './menu-item.style.scss';
 
 const MenuItem = ({title, id, imageUrl, size}) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} 
+    <div  
     className= {`menu-item ${size}`}>
+        <div className='background-image' style={{
+        backgroundImage: `url(${imageUrl})`
+    }} />
     <div className="content">
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
